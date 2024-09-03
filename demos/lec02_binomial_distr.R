8ff316280e1a913678da1f3e08701cabe1a23d80
Adam Kapelner <kapelner@gmail.com>
2024-09-03 15:22:14 -0400

    class demo with n=28

--- demos/lec02_binomial_distr.R
+++ demos/lec02_binomial_distr.R
@@ -1,11 +1,11 @@
-library(ggplot2)
+pacman::p_load(ggplot2)
 
-n = 21
+n = 28
 t = 0 : n
 thetahathat = t / n
 theta = .524
 pmf_theta = dbinom(t, n, theta)
 cbind(t, thetahathat, pmf_theta)
 
-ggplot(data.frame(thetahathat = as.factor(round(thetahathat, 4)), pmf_theta = pmf_theta)) + 
+ggplot(data.frame(thetahathat = as.factor(round(thetahathat, 3)), pmf_theta = pmf_theta)) + 
   geom_bar(aes(x = thetahathat, y = pmf_theta), stat = "identity")
