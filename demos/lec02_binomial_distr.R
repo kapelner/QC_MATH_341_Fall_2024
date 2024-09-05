1982ebbf83241709d7925cad9c7be6cd746ae7fd
Adam Kapelner <kapelner@gmail.com>
2024-09-05 13:16:52 -0400

    binomial exact test demo improved

--- demos/lec02_binomial_distr.R
+++ demos/lec02_binomial_distr.R
@@ -1,11 +1,32 @@
 pacman::p_load(ggplot2)
 
-n = 28
-t = 0 : n
-thetahathat = t / n
 theta = .524
+n = 28
+t_hathat = 0 : n
+theta_hathat = t / n
 pmf_theta = dbinom(t, n, theta)
-cbind(t, thetahathat, pmf_theta)
+left_tail_probs = pbinom(t, n, theta)
+right_tail_probs = 1 - pbinom(t - 1, n, theta)
 
-ggplot(data.frame(thetahathat = as.factor(round(thetahathat, 3)), pmf_theta = pmf_theta)) + 
-  geom_bar(aes(x = thetahathat, y = pmf_theta), stat = "identity")
+info = data.frame(
+  t_hathat = as.factor(t_hathat), 
+  theta_hathat = as.factor(round(theta_hathat, 3)), 
+  pmf_theta,
+  left_tail_probs,
+  right_tail_probs,
+  rejection_region = theta_hathat <= 0.3219 | theta_hathat >= 0.714
+)
+sum(info$pmf_theta[info$rejection_region])
+
+ggplot(info) + 
+  geom_bar(aes(x = t_hathat, y = pmf_theta), stat = "identity")
+ggplot(info) + 
+  geom_bar(aes(x = theta_hathat, y = pmf_theta), stat = "identity")
+ggplot(info) + 
+  geom_bar(aes(x = theta_hathat, y = left_tail_probs), stat = "identity") +
+  geom_hline(yintercept = 0.025, col = "red")
+ggplot(info) + 
+  geom_bar(aes(x = theta_hathat, y = right_tail_probs), stat = "identity") +
+  geom_hline(yintercept = 0.025, col = "red")
+ggplot(info) + 
+  geom_bar(aes(x = theta_hathat, y = pmf_theta, fill = rejection_region), stat = "identity")
